998a6c2fc1a652e8df23bf24af5585ca24616110
Quentin DESBIN <qdesbin@iobeya.com>
2023-11-16 11:07:17 +0100

    ✅ tests mocking for service with db

--- library-express/test/controllers/books.test.js
+++ library-express/test/controllers/books.test.js
@@ -1,18 +1,20 @@
 const request = require('supertest');
 const app = require('../../app');
-const booksService = require('../../services/books')
-//const books = require('../../models').books
+const booksService = require('../../services/db/books')
 
 // Globals mocks
 const mockedBooks = [{id: 1, title: "test"}, {id: 2, title: "toto"}, {id: 3, title: "test2"}, {id: 4, title: "test2"}]
-jest.mock('../../services/books');
+jest.mock('../../services/db/books');
 booksService.getBooks.mockReturnValueOnce(mockedBooks)
 booksService.addBook.mockReturnValueOnce(true)
 
 describe('get books endpoint', () => {
 
     it('should return collection of books', async () => {
+        // When
         const resp = await request(app).get('/books');
+
+        // Then
         expect(resp.statusCode).toEqual(200);
         expect(resp.body).not.toBeNull();
         expect(resp.body).toHaveProperty('data');
@@ -22,7 +24,10 @@ describe('get books endpoint', () => {
     });
 
     it('should fail when book not found', async () => {
+        // When
         const resp = await request(app).get('/books/10');
+        
+        // Then
         expect(resp.statusCode).toEqual(404);
         expect(resp.body).not.toBeNull();
         expect(resp.body).not.toHaveProperty('data');
@@ -34,10 +39,13 @@ describe('get books endpoint', () => {
 
 describe('post books endpoint', () => {
     it('should add book successfully', async () => {
+        // When
         const resp = await request(app).post('/books').set('Authorization', 'Bearer TOKEN').send({
             title: 'test',
             date: '2022-10-10'
         });
+
+        // Then
         expect(resp.statusCode).toEqual(201);
         expect(resp.body).not.toBeNull();
         expect(resp.body).toHaveProperty('success');
@@ -45,9 +53,12 @@ describe('post books endpoint', () => {
     });
 
     it('should fail when property to add book is missing', async () => {
+        // When
         const resp = await request(app).post('/books').set('Authorization', 'Bearer TOKEN').send({
             title: 'test'
         });
+
+        // Then
         expect(resp.statusCode).toEqual(400);
         expect(resp.body).not.toBeNull();
         expect(resp.body).toHaveProperty('success');
