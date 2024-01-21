5c009c25f9624baca8dfcb51a409750610c5c3ca
Quentin DESBIN <qdesbin@iobeya.com>
2024-01-21 10:00:50 +0100

    ✅ fix test

--- library-express/test/controllers/books.test.js
+++ library-express/test/controllers/books.test.js
@@ -1,41 +1,61 @@
-const request = require('supertest');
-const app = require('../../app');
+const request = require('supertest')
+const app = require('../../app')
 const booksService = require('../../services/db/books')
 
 // Globals mocks
 const mockedBooks = [{id: 1, title: "test"}, {id: 2, title: "toto"}, {id: 3, title: "test2"}, {id: 4, title: "test2"}]
-jest.mock('../../services/db/books');
-booksService.getBooks.mockReturnValueOnce(mockedBooks)
+jest.mock('../../services/db/books')
 booksService.addBook.mockReturnValueOnce(true)
 
 describe('get books endpoint', () => {
 
     it('should return collection of books', async () => {
+        // Mock
+        booksService.getBooks.mockReturnValueOnce(mockedBooks)
+
         // When
-        const resp = await request(app).get('/books');
+        const resp = await request(app).get('/books')
 
         // Then
-        expect(resp.statusCode).toEqual(200);
-        expect(resp.body).not.toBeNull();
-        expect(resp.body).toHaveProperty('data');
-        expect(resp.body).toHaveProperty('success');
-        expect(resp.body.success).toBeTruthy();
-        expect(resp.body.data).toHaveLength(4);
-    });
+        expect(resp.statusCode).toEqual(200)
+        expect(resp.body).not.toBeNull()
+        expect(resp.body).toHaveProperty('data')
+        expect(resp.body).toHaveProperty('success')
+        expect(resp.body.success).toBeTruthy()
+        expect(resp.body.data).toHaveLength(4)
+    })
 
-    it('should fail when book not found', async () => {
+    it('should works when book is found', async () => {
+        // Mock
+        booksService.getBookById.mockReturnValueOnce(mockedBooks[0])
+
         // When
-        const resp = await request(app).get('/books/10');
+        const resp = await request(app).get('/books/10')
         
         // Then
-        expect(resp.statusCode).toEqual(404);
-        expect(resp.body).not.toBeNull();
-        expect(resp.body).not.toHaveProperty('data');
-        expect(resp.body).toHaveProperty('success');
-        expect(resp.body).toHaveProperty('message');
-        expect(resp.body.success).toBeFalsy();
-    });
-});
+        expect(resp.statusCode).toEqual(200)
+        expect(resp.body).not.toBeNull()
+        expect(resp.body).toHaveProperty('data')
+        expect(resp.body).toHaveProperty('success')
+        expect(resp.body.success).toBeTruthy()
+    })
+
+    it('should fail when book not found', async () => {
+        // Mock
+        booksService.getBookById.mockReturnValueOnce(false)
+
+        // When
+        const resp = await request(app).get('/books/10')
+        
+        // Then
+        expect(resp.statusCode).toEqual(404)
+        expect(resp.body).not.toBeNull()
+        expect(resp.body).not.toHaveProperty('data')
+        expect(resp.body).toHaveProperty('success')
+        expect(resp.body).toHaveProperty('message')
+        expect(resp.body.success).toBeFalsy()
+    })
+})
 
 describe('post books endpoint', () => {
     it('should add book successfully', async () => {
@@ -43,26 +63,26 @@ describe('post books endpoint', () => {
         const resp = await request(app).post('/books').set('Authorization', 'Bearer TOKEN').send({
             title: 'test',
             date: '2022-10-10'
-        });
+        })
 
         // Then
-        expect(resp.statusCode).toEqual(201);
-        expect(resp.body).not.toBeNull();
-        expect(resp.body).toHaveProperty('success');
-        expect(resp.body.success).toBeTruthy();
-    });
+        expect(resp.statusCode).toEqual(201)
+        expect(resp.body).not.toBeNull()
+        expect(resp.body).toHaveProperty('success')
+        expect(resp.body.success).toBeTruthy()
+    })
 
     it('should fail when property to add book is missing', async () => {
         // When
         const resp = await request(app).post('/books').set('Authorization', 'Bearer TOKEN').send({
             title: 'test'
-        });
+        })
 
         // Then
-        expect(resp.statusCode).toEqual(400);
-        expect(resp.body).not.toBeNull();
-        expect(resp.body).toHaveProperty('success');
-        expect(resp.body).toHaveProperty('message');
-        expect(resp.body.success).toBeFalsy();
-    });
-});
+        expect(resp.statusCode).toEqual(400)
+        expect(resp.body).not.toBeNull()
+        expect(resp.body).toHaveProperty('success')
+        expect(resp.body).toHaveProperty('message')
+        expect(resp.body.success).toBeFalsy()
+    })
+})
